96a7a4270c44ae12b4c31f44be316a274f0d8934
JMoulderGW <56315594+JMoulderGW@users.noreply.github.com>
2020-02-17 17:06:19 +0000

    Update gettingstarted-ubuntu-bashscript.sh

--- Getting Started/gettingstarted-ubuntu-bashscript.sh
+++ Getting Started/gettingstarted-ubuntu-bashscript.sh
@@ -23,15 +23,15 @@ echo "Cloning the Glasswall libraries"
 git clone https://github.com/filetrust/SDK-Evaluation-Version-2.x.git
 
 ## copy SDK into folders ##
-ln -s /home/glasswall/Desktop/glasswall/Lib/libglasswall_core2.so /usr/lib/libglasswall_core2.so.2 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libQt5Concurrent.so /usr/lib/libQt5Concurrent.so.5 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libQt5Core.so /usr/lib/libQt5Core.so.5 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libquazip.so /usr/lib/libquazip.so.1 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libQt5Xml.so /usr/lib/libQt5Xml.so.5 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libicui18n.so /usr/lib/libicui18n.so.56 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libicuuc.so /usr/lib/libicuuc.so.56 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libicudata.so /usr/lib/libicudata.so.56 && \
-ln -s /home/glasswall/Desktop/glasswall/Lib/libQt5Gui.so /usr/lib/libQt5Gui.so.5 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libglasswall_core2.so /usr/lib/libglasswall_core2.so.2 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libQt5Concurrent.so /usr/lib/libQt5Concurrent.so.5 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libQt5Core.so /usr/lib/libQt5Core.so.5 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libquazip.so /usr/lib/libquazip.so.1 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libQt5Xml.so /usr/lib/libQt5Xml.so.5 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libicui18n.so /usr/lib/libicui18n.so.56 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libicuuc.so /usr/lib/libicuuc.so.56 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libicudata.so /usr/lib/libicudata.so.56 && \
+ln -s /home/glasswall/Desktop/glasswall/SDK-Evaluation-Version-2.x/Lib/libQt5Gui.so /usr/lib/libQt5Gui.so.5 && \
 ldconfig /usr/lib
 
 ##
